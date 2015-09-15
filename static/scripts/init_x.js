5bb7418c3b41b4c76f6ed86d43d4c63c43fe7818
saulo <sauloal@gmail.com>
2015-09-15 01:36:34 +0000

    multiple repos

--- static/scripts/init_x.js
+++ static/scripts/init_x.js
@@ -1,13 +1,19 @@
-/* global get_usage  */
-/* global CONTAINER  */
-/* global get_tables */
+/* global get_usage   */
+/* global CONTAINER   */
+/* global get_tables  */
+/* global DOCKER_DATA */
 
 $(document).ready(function(){
     console.log('document is ready');
     
         get_usage("num_sessions", "num_views");
 
-        var username = 'biodckr';
-
-        get_tables(CONTAINER, username);
+        //var username = 'biodckr';
+        
+        console.log(DOCKER_DATA);
+        var usernames = Object.keys(DOCKER_DATA);
+        var username  = usernames.join('|');
+        if ( usernames.length > 0 ) {
+            get_tables(CONTAINER, username);
+        }
 })
